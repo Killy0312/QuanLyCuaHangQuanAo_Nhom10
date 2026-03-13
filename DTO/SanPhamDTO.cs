fda7c395f80274f15c1b6a0df78e183d66e2b282
Tuan Kiet <kietchaunguyen17@gmail.com>
2026-03-14 02:10:19 +0700

    tao SanPhamDTO chua cac du lieu cua san pham

---
+++ DTO/SanPhamDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class SanPhamDTO
+    {
+        public int MaSP { get; set; }
+        public string TenSP { get; set; }
+        public int MaLoai { get; set; }
+        public string KichCo { get; set; }
+        public string MauSac { get; set; }
+        public decimal GiaBan { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
