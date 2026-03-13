005a9d601170e98eee33da5509e78e0391822507
phuclaban <phuclaban@DESKTOP-BV31JU0>
2026-03-13 21:45:43 +0700

    Bus:GioHang DTO:Chitiethoadon+hoadon

---
+++ DTO/ChiTietHoaDon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyShopQuanAo.DTO
+{
+    public class ChiTietHoaDon
+    {
+        public int MaHD { get; set; }
+        public int MaSP { get; set; }
+        public int SoLuong { get; set; }
+        public double DonGia { get; set; }
+        
+        public double ThanhTien
+        {
+            get { return SoLuong * DonGia; }
+        }
+    }
+}
