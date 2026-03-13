110f2fe055a6be86294278e0201baeb182c19e8c
XuanGranbird <nguyenxuantv2212@gmail.com>
2026-03-13 22:00:16 +0700

    Add files via upload

---
+++ QuanLyCuaHangQuanAo_Nhom10-master/QuanLyCuaHangQuanAo_Nhom10-master/GUI/FrmRegister.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyShopQuanAo
+{
+    public partial class FrmRegister : Form
+    {
+        public FrmRegister()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void FrmRegister_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FrmLogin login = new FrmLogin();
+            login.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ QuanLyCuaHangQuanAo_Nhom10-master/QuanLyCuaHangQuanAo_Nhom10-master/GUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace QuanLyShopQuanAo
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Dòng này gọi đúng tên file frmMain lên
+            Application.Run(new FrmWelcome());
+        }
+    }
+}
