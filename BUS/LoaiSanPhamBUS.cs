39f099d99dd31b83ca730e50c36c174769fcc5a9
Tuan Kiet <kietchaunguyen17@gmail.com>
2026-03-15 01:12:36 +0700

    Thêm lớp BUS để quản lý sản phẩm

---
+++ BUS/LoaiSanPhamBUS.cs
@@ -0,0 +1,20 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class LoaiSanPhamBUS
+    {
+        LoaiSanPhamDAL dal = new LoaiSanPhamDAL();
+
+        public DataTable GetLoai()
+        {
+            return dal.GetLoaiSanPham();
+        }
+    }
+}
---
+++ BUS/SanPhamBUS.cs
@@ -0,0 +1,43 @@
+﻿using DAL;
+using DTO;
+using QuanLyShopQuanAo.DAL;
+using QuanLyShopQuanAo.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class SanPhamBUS
+    {
+        SanPhamDAL dal = new SanPhamDAL();
+
+        public DataTable GetSanPham()
+        {
+            return dal.GetAll();
+        }
+
+        public bool Them(SanPhamDTO sp)
+        {
+            return dal.Insert(sp) > 0;
+        }
+
+        public bool Sua(SanPhamDTO sp)
+        {
+            return dal.Update(sp) > 0;
+        }
+
+        public bool Xoa(int ma)
+        {
+            return dal.Delete(ma) > 0;
+        }
+
+        public DataTable TimKiem(string ten)
+        {
+            return dal.Search(ten);
+        }
+    }
+}
